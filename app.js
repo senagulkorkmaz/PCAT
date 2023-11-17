b8584d54982030f88a5087b1a748f2bdf4a01941
senagulkorkmaz <senagulkorkmazz@gmail.com>
2023-11-17 17:24:15 +0300

    nodemon öğrenildi.

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const fn = function () {
-    console.log("Function Working");
-};
+const express = require('express')
+
+const app = express();
+
+app.get('/', (req, res) => {
+    const photo = {
+        id: 1,
+        name: "Photo Name",
+        description: "Photo Description"
+    }
+    res.send(photo)
+    // res.send('MERHABA')
+})
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Sunucu ${port} portunda başlatıldı..`);
+})
