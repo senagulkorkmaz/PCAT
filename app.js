a870c621782da07dccdb646af6a7cda22fd0bc3f
senagulkorkmaz <senagulkorkmazz@gmail.com>
2023-11-18 10:01:37 +0300

    Static files upload

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 const express = require('express')
+const path = require('path')
 
 const app = express();
 
+
+// const myLogger = (req, res, next) => {
+//     console.log('Middleware Log 1');
+//     next();
+// }
+
+// const myLogger2 = (req, res, next) => {
+//     console.log('Middleware Log 2');
+//     next();
+// }
+
+app.use(express.static('public'))
+// app.use(myLogger)
+// app.use(myLogger2)
+
 app.get('/', (req, res) => {
-    const photo = {
-        id: 1,
-        name: "Photo Name",
-        description: "Photo Description"
-    }
-    res.send(photo)
+    res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+    // const photo = {
+    //     id: 1,
+    //     name: "Photo Name",
+    //     description: "Photo Description"
+    // }
+    // res.send(photo)
     // res.send('MERHABA')
 })
 
