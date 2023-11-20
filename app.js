e02683899481adf6c1a6027bb5a874f0ff63b006
senagulkorkmaz <senagulkorkmazz@gmail.com>
2023-11-20 11:40:55 +0300

    Tarayıcıdan veri gönderme işlemi yapıldı.

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ app.set("view engine", "ejs");
 // }
 
 // MIDDLEWARES
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // app.use(myLogger)
 // app.use(myLogger2)
 
@@ -38,16 +40,20 @@ app.get('/', (req, res) => {
 })
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index');
 })
 
 app.get('/about', (req, res) => {
-    res.render('about')
+    res.render('about');
 })
 
 app.get('/add', (req, res) => {
-    res.render('add')
+    res.render('add');
 })
+app.post('/photos', (req, res) => {
+    console.log(req.body);
+    res.redirect('/')
+});
 
 
 const port = 3000;
